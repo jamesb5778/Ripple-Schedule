39e0bb2afeada0074487efda9c15ceff82046505
James Bell <jamesbell@Jamess-MacBook-Pro.local>
2021-11-13 11:45:59 -0700

    create path for managers page

---
+++ main_app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path for the login page
+    # path for the home page.
+    path('', views.Home.as_view(), name = "home"),
+    # path for the schedule Page (ALL)
+    # path for administrators page (Admin Only)
+    path('manager_page/', views.Manager_Page.as_view(), name = "manager_page"),
+    # path for create, update, delete and post(submit) a schedule (Admin Only)
+    
+]
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
 
 # Create your views here.
+#view for the homepage
+class Home(View):
+    def get(self, request):
+        return HttpResponse("Schedule Home Page, view schedule, make schedule, etc")
+
+# view for the managers page
+class Manager_Page(View):
+    def get(self, request):
+        return HttpResponse("Schedule Manager Page, view schedule, make schedule")
