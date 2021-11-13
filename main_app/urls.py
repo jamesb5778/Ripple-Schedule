d06e30d2a1dde44dcb1630b026925c20130fa6ad
James Bell <jamesbell@Jamess-MacBook-Pro.local>
2021-11-13 15:20:18 -0700

    remove main_app from settings.py, TEMPLATES causing import error.

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.Home.as_view(), name = "home"),
     # path for the schedule Page (ALL)
     # path for administrators page (Admin Only)
-    path('manager_page/', views.Manager_Page.as_view(), name = "manager_page"),
+    path('manager_page/', views.Manager_Page.as_view(), name = "manager_page")
     # path for create, update, delete and post(submit) a schedule (Admin Only)
     
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 # Create your views here.
 #view for the homepage
-class Home(View):
-    def get(self, request):
-        return HttpResponse("Schedule Home Page, view schedule, make schedule, etc")
-
+class Home(TemplateView):
+    template_name="home.html"
+    
 # view for the managers page
 class Manager_Page(View):
     def get(self, request):
