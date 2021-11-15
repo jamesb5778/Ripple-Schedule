0a44bfec55a1e739194b292a01e5d3ce6640eed8
James Bell <jamesbell@Jamess-MacBook-Pro.local>
2021-11-14 18:15:32 -0700

    Employees model create, superuser create

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+# Model for New Employee's
+class Employee(models.Model):
+    EmpNumber = models.IntegerField()
+    FirstName = models.CharField(max_length=100)
+    LastName = models.CharField(max_length=100)
+    JobTitle = models.CharField(max_length=100)
+    Username = models.CharField(max_length=100)
+    Password = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.LastName
+    
+    class Meta:
+        ordering = ["LastName"]
+    
